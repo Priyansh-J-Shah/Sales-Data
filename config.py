d9fc9a5bf70e180cb2728c7bcc71222f46bc2feb
Priyansh-J-Shah <spriyansh78@gmail.com>
2022-10-27 13:29:19 +0530

    Project Configuration

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+CSV_FILE_PATH = os.path.join("Model","Sales_data.csv")
+
+MODEL_FILE_PATH = os.path.join("Model","Linear_Regression_Sales_Data.pkl")
+
+JSON_FILE_PATH = os.path.join("Model","Linear_Regression_Sales_Data.json")
